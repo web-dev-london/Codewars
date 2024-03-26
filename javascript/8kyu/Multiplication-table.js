404e11446c325184ec489798fdeb5c28e418fdaf
Murod Akhmedov <murod.london@gmail.com>
2024-03-26 22:34:10 +0000

    Solution of multiplication table

---
+++ javascript/8kyu/Multiplication-table.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/* const number = 3;
+
+function multiply(number) {
+  let output = [];
+  for (let i = 1; i <= number; i++) {
+    let inner = [];
+    for (let j = 1; j <= number; j++) {
+      inner.push(i * j);
+    }
+    output.push(inner);
+  }
+  return output;
+}
+multiply(number);
+ */
